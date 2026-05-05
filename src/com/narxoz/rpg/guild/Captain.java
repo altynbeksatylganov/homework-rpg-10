2d5909d3deaaab7c1cd757f32bdb9ccc8235d96f
Altynbek Satylganov <altynbeksatylganov92@gmail.com>
2026-05-05 18:18:19 +0500

    Implement captain mediator behavior

--- src/com/narxoz/rpg/guild/Captain.java
+++ src/com/narxoz/rpg/guild/Captain.java
@@ -1,21 +1,35 @@
 package com.narxoz.rpg.guild;
 
-/**
- * Guild officer responsible for orders and mission coordination.
- */
+import java.util.List;
+
 public class Captain extends GuildMember {
 
     public Captain(String name, GuildMediator mediator) {
         super(name, mediator);
     }
 
+    @Override
+    protected List<String> subscriptions() {
+        return List.of("orders", "scouting", "supplies", "healing", "rewards", "urgent", "lore", "curse", "history");
+    }
+
     public void issueOrder(String topic, String payload) {
-        // TODO: send a command message through the mediator.
         getMediator().dispatch(topic, this, payload);
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        if ("urgent".equals(topic)) {
+            System.out.println("[Captain] " + getName()
+                    + " marks this as top priority after message from "
+                    + senderName(from) + ": " + payload);
+        } else if ("scouting".equals(topic)) {
+            System.out.println("[Captain] " + getName()
+                    + " updates the battle plan using scouting report: " + payload);
+        } else {
+            System.out.println("[Captain] " + getName()
+                    + " reviews topic '" + topic + "' from "
+                    + senderName(from) + ": " + payload);
+        }
     }
 }
