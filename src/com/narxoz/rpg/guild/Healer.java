48b1825748aa23eb2a1d0d012af4e2ebe2bb688f
Altynbek Satylganov <altynbeksatylganov92@gmail.com>
2026-05-05 18:17:16 +0500

    Implement healer mediator behavior

--- src/com/narxoz/rpg/guild/Healer.java
+++ src/com/narxoz/rpg/guild/Healer.java
@@ -1,21 +1,37 @@
 package com.narxoz.rpg.guild;
 
-/**
- * Guild officer responsible for wounds, potions, and recovery plans.
- */
+import java.util.List;
+
 public class Healer extends GuildMember {
 
     public Healer(String name, GuildMediator mediator) {
         super(name, mediator);
     }
 
+    @Override
+    protected List<String> subscriptions() {
+        return List.of("orders", "healing", "urgent", "curse", "scouting");
+    }
+
     public void prepareAid(String topic, String payload) {
-        // TODO: send a healing message through the mediator.
         getMediator().dispatch(topic, this, payload);
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        if ("healing".equals(topic)) {
+            System.out.println("[Healer] " + getName()
+                    + " prepares recovery plan after message from "
+                    + senderName(from) + ": " + payload);
+        } else if ("curse".equals(topic)) {
+            System.out.println("[Healer] " + getName()
+                    + " prepares antidotes and blessing salts: " + payload);
+        } else if ("urgent".equals(topic)) {
+            System.out.println("[Healer] " + getName()
+                    + " brings emergency bandages and mana tonics.");
+        } else {
+            System.out.println("[Healer] " + getName()
+                    + " records council note from " + senderName(from) + ": " + payload);
+        }
     }
 }
